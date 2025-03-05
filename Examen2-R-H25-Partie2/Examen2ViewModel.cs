cee40a86713315e7335129c44926ef71f59652bc
MM <maryse.mongeau@cegepoutaouais.qc.ca>
2025-03-05 16:49:59 -0500

    Ajoutez des fichiers projet.

---
+++ Examen2-R-H25-Partie2/Examen2ViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Examen2_R_H25_Partie2
+{
+    public class Examen2ViewModel : INotifyPropertyChanged
+    {
+        // TODO : Ajouter des propriétés avec encapsulation pour le binding
+        // Les propriétés sont 2 ObservableCollection : Bibliotheque et Panier,
+        // et 2 Livres : celui sélectionné dans le DataGrid et celui sélectionné dans le ListBox.
+        // Ajouter aussi les 3 ICommand pour le binding sur les boutons et le menu.
+
+
+        public Examen2ViewModel()
+        {
+            // TODO : Lire le fichier JSON pour initialiser et remplir la Bibliotheque; 
+
+
+            // TODO : Initialiser le panier à une liste vide
+
+
+            // TODO : Initialiser les RelayCommand pour attacher les boutons à leurs gestionnaires d'évènements
+
+
+        }
+
+        // Gestionnaires d'évènements
+        private void Emprunter()
+        {
+            // TODO : Ajouter le livre selectionné au panier
+
+        }
+        private void Retourner()
+        {
+            // TODO : Supprimer le livre à retourner du panier
+
+        }
+
+        private void Save()
+        {
+            // TODO : Sauvegarder la Bibliotheque dans le fichier json
+
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ Examen2-R-H25-Partie2/Livre.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen2_R_H25_Partie2
+{
+    public class Livre
+    {
+        private string _isbn;
+        public string Titre { get; set; }
+        public string Auteur { get; set; }
+
+        private float _prix;
+
+        private int _nombrePages;
+        
+        private int _anneePublication;
+
+        public string ISBN
+        {
+            get => _isbn;
+            set
+            {
+                string chiffres = value.Replace("-", "");
+                if (!((chiffres.Length==10 || chiffres.Length == 13)
+                    && long.TryParse(chiffres, out _)))
+                    throw new ArgumentException("ISBN invalide. Il doit contenir 10 ou 13 chiffres avec ou sans tirets.");
+                _isbn = value;
+            }
+        }
+
+        public float Prix
+        {
+            get => _prix;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Le prix ne peut pas être négatif.");
+                _prix = value;
+            }
+        }
+
+        public int NombrePages
+        {
+            get => _nombrePages;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Le nombre de pages doit être positif.");
+                _nombrePages = value;
+            }
+        }
+
+        public int AnneePublication
+        {
+            get => _anneePublication;
+            set
+            {
+                if (value < 1450 || value > DateTime.Now.Year)
+                    throw new ArgumentException("L'année de publication doit être comprise entre 1450 et l'année actuelle.");
+                _anneePublication = value;
+            }
+        }
+        public Livre()
+        {
+        }
+        public Livre(string isbn, string titre, string auteur, float prix, int nombrePages, int anneePublication)
+        {
+            ISBN = isbn;
+            Titre = titre;
+            Auteur = auteur;
+            Prix = prix;
+            NombrePages = nombrePages;
+            AnneePublication = anneePublication;
+        }
+
+        public override string ToString()
+        {
+            return $"Livre: {Titre} | Auteur: {Auteur} | ISBN: {ISBN} | Prix: {Prix}$ | Pages: {NombrePages} | Année: {AnneePublication}";
+        }
+    }
+}
